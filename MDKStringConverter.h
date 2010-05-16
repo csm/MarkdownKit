6476fbbc4ebe9c137b34bc85babfed9113305555
Casey Marshall <casey.s.marshall@gmail.com>
2010-05-16 16:47:03 -0700

    Initial checkin.

---
+++ MDKStringConverter.h
@@ -0,0 +1,16 @@
+//
+//  MDKStringConverter.h
+//  MarkdownKit
+//
+//  Created by Casey Marshall on 5/16/10.
+//  Copyright 2010 Modal Domains. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MDKStringConverter : NSObject
+{
+}
+
+@end
---
+++ MDKStringConverter.m
@@ -0,0 +1,14 @@
+//
+//  MDKStringConverter.m
+//  MarkdownKit
+//
+//  Created by Casey Marshall on 5/16/10.
+//  Copyright 2010 Modal Domains. All rights reserved.
+//
+
+#import "MDKStringConverter.h"
+
+
+@implementation MDKStringConverter
+
+@end
